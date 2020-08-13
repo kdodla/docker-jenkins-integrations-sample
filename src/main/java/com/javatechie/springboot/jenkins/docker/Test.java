ac8288b28b93e2969eb5fa4fc82fd493d48fcf77
Kalpana <kdodla1983@gmail.com>
2020-08-13 11:15:18 -0400

    second commit

---
+++ src/main/java/com/javatechie/springboot/jenkins/docker/Test.java
@@ -0,0 +1,10 @@
+package com.javatechie.springboot.jenkins.docker;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hellooo");
+	}
+
+}
